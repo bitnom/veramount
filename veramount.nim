4b1452bcf5c76366f08a3d6d7fdfb03a788bd49c
Tom Ashley <tom.ashley@aicoders.com>
2017-07-07 10:39:55 -0400

    Create veramount.nim

---
+++ veramount.nim
@@ -0,0 +1,36 @@
+import
+    strutils,
+    os,
+    osproc
+
+var
+    procMounts:string
+    lineSplit:seq[string]
+    keyDrive:string
+    keyFile:string
+    keyInserted:bool = false
+    mounted:bool = false
+    fired:bool = false
+
+while true:
+    if not mounted:
+        procMounts = readFile("/proc/mounts")
+        for line in lines "mounts.txt":
+            lineSplit = line.split("=")
+            if lineSplit.len() == 2:
+                keyDrive = lineSplit[1]
+                keyFile = keyDrive & "/keydrive"
+                if keyFile.existsFile():
+                    keyInserted = true
+                else:
+                    keyInserted = false
+            lineSplit = line.splitWhitespace()
+            if lineSplit.len() == 3 and keyInserted:
+                discard startProcess("veracrypt -t --non-interactive --mount -k " & keyDrive & "/" & lineSplit[0] & " " & lineSplit[1] & " " & lineSplit[2], "/usr/bin", [], nil, {poEvalCommand})
+                fired = true
+                mounted = true
+                echo "veracrypt -t --non-interactive --mount -k " & keyDrive & "/" & lineSplit[0] & " " & lineSplit[1] & " " & lineSplit[2]
+        if fired:
+            discard startProcess("mergerfs -o defaults,allow_other,use_ino /media/pool\\* /media/merger", "/usr/bin", [], nil, {poEvalCommand})
+            fired = false
+    sleep(1000)
